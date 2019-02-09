baf50ab8c0331b2df1c1a37e02615cd16585c689
Daniel Ran <danielr1735@gmail.com>
2019-02-09 11:26:20 -0500

    moved gameLogic to separate file

--- src/Game.js
+++ src/Game.js
@@ -1,56 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Board from './Board'
-import { randFromOneTo, randFromZeroTo, selectRandomNums, getLine } from './helpers.js'
+import { winningLines, preVictoryPositions, isPlayerFirst, initialPosition, willLineWin, findWinnindLine, findWinningSquare } from './gameLogic.js'
 
 
 const Game = ({id, active}) => {
 
-  const winningLines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-  const preVictoryPositions = (next) => [[next, next, null], [next, null, next], [null, next, next]]
-
-  const isPlayerFirst = randFromZeroTo(1) === 0
-
-  const initialPosition = (bool) => {
-    let position = Array(9).fill(null)
-    if (!bool) {
-        position[randFromZeroTo(8)] = 'X'
-    }
-    return position
-  }
-
-  // const winningMove = (currentPosition, next) => {
-  //   let result = winningLines.find(line => {
-  //     let currentLinePosition = getLine(currentPosition, line)
-  //
-  //     return preVictoryPositions(next).find(position => {
-  //
-  //       return position[0] == currentLinePosition[0] && position[1] == currentLinePosition[1] && position[2] == currentLinePosition[2]
-  //     })
-  //   })
-  //   console.log("result = ", result)
-  //   return result
-  // }
-
-  const willLineWin = (line, position, next) => {
-    let result = preVictoryPositions(next).find(pvPosition => pvPosition[0] == position[line[0]] && pvPosition[1] == position[line[1]] && pvPosition[2] == position[line[2]])
-    return result
-  }
-
-  const findWinningLine = (position, next) => {
-    let result = winningLines.find(line => willLineWin(line, position, next))
-    return result
-  }
-
-  const findWinningSquare = (position, next) => {
-    let winningLine = findWinningLine(position, next)
-    let winningSquare = winningLine && winningLine.find(i => position[i] === null)
-    return winningSquare
-  }
-
-  // const winningSquare = (position, next) => {
-  //   willPositionWin(position, next)
-  // }
 
   const [gameState, setGameState] = useState({
     playerFirst: isPlayerFirst,
---
+++ src/gameLogic.js
@@ -0,0 +1,31 @@
+import { randFromOneTo, randFromZeroTo, selectRandomNums, getLine } from './helpers.js'
+
+// game constants
+const winningLines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+const preVictoryPositions = (next) => [[next, next, null], [next, null, next], [null, next, next]]
+
+// beginning player / initial position
+const isPlayerFirst = randFromZeroTo(1) === 0
+const initialPosition = (bool) => {
+  let position = Array(9).fill(null)
+  if (!bool) {
+      position[randFromZeroTo(8)] = 'X'
+  }
+  return position
+}
+
+// game logic
+const willLineWin = (line, position, next) => {
+  return preVictoryPositions(next).find(pvPosition => pvPosition[0] == position[line[0]] && pvPosition[1] == position[line[1]] && pvPosition[2] == position[line[2]])
+}
+
+const findWinningLine = (position, next) => {
+  return winningLines.find(line => willLineWin(line, position, next))
+}
+
+const findWinningSquare = (position, next) => {
+  let winningLine = findWinningLine(position, next)
+  return winningLine && winningLine.find(i => position[i] === null)
+}
+
+export { winningLines, preVictoryPositions, isPlayerFirst, initialPosition, willLineWin, findWinningLine, findWinningSquare }
