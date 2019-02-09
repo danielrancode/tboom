61dc36db6fa8fae1fb20a514aa24b61ede93e7ae
Daniel Ran <danielr1735@gmail.com>
2019-02-09 11:52:42 -0500

    refactor as to not prematurely mutate game state

--- src/Game.js
+++ src/Game.js
@@ -5,7 +5,6 @@ import { winningLines, preVictoryPositions, isPlayerFirst, initialPosition, will
 
 const Game = ({id, active}) => {
 
-
   const [gameState, setGameState] = useState({
     playerFirst: isPlayerFirst,
     position: initialPosition(isPlayerFirst),
@@ -15,10 +14,16 @@ const Game = ({id, active}) => {
 
   const changeGameState = (e) => {
     let squareId = e.target.id
-    let position = gameState.position
+    let position = [...gameState.position]
+    let oldNext = gameState.next
+    let newNext = gameState.next == 'X' ? 'O' : 'X'
+
+
+    // check if sqaure is empty
+
     if (position[squareId] === null) {
+      // put in move
       position.splice(squareId, 1, gameState.next)
-      let newNext = gameState.next == 'X' ? 'O' : 'X'
 
       setGameState({
         ...gameState,
@@ -27,17 +32,20 @@ const Game = ({id, active}) => {
       })
 
       let winningSquare = findWinningSquare(position, newNext)
+      let playerWinningSquare = findWinningSquare(position, oldNext)
 
       if (winningSquare >= 0 ) {
-        console.log("hitting")
-        position[winningSquare] = newNext
         setTimeout( () => {
+          position[winningSquare] = newNext
+
           setGameState({
           ...gameState,
           position: position,
           status: 'won'
         })}, 1000)
       }
+
+
     }
 
   }
