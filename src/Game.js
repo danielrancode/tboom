431abe6ac1f2e7f9e3ca7d63af94aaddd5ac0f79
Daniel Ran <danielr1735@gmail.com>
2019-02-10 15:10:47 -0500

    refactored useState

--- src/Game.js
+++ src/Game.js
@@ -1,96 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import Board from './Board'
-import { winningLines, preVictoryPositions, isPlayerFirst, initialPosition, willLineWin, findWinnindLine, findWinningSquare, isGameWon, isDraw, randomEmptySquare } from './gameLogic.js'
+import { winningLines, preVictoryPositions, isPlayerFirst, initialPosition, willLineWin, findWinningLine, findWinningSquare, isGameWon, isDraw, randomEmptySquare } from './gameLogic.js'
 
 
 const Game = ({id, active}) => {
-
-  // const [gameState, setGameState] = useState({
-  //   playerFirst: isPlayerFirst,
-  //   position: initialPosition(isPlayerFirst),
-  //   next: isPlayerFirst ? 'X' : 'O',
-  //   status: 'ticking'
-  // })
 
   let first = isPlayerFirst()
 
   const [playerFirst, setPlayerFirst] = useState(first)
   const [position, setPosition] = useState(initialPosition(first))
   const [next, setNext] = useState(first ? 'X' : 'O')
+  const [previous, setPrevious] = useState('')
   const [status, setStatus] = useState('player')
 
   useEffect(() => {opponentMove()}, [position, next, status])
 
-  const changeGameState = (e) => {
 
+  // this function is executed when player clicks a square
+  const changeGameState = (e) => {
+    // get square number
     let id = e.target.id
 
+    // if it is the player's turn to play and the square is empty: make move.
     if (status == 'player' && position[id] == null) {
+      // create and update new 'position' array
       let newPosition = [...position]
       newPosition[id] = next
-      // console.log(newPosition)
-      // put in move
+
+      // update state
       setPosition(newPosition)
       setNext(next == 'X' ? 'O' : 'X')
+      setPrevious(next == 'X' ? 'X' : 'O')
       setStatus('opponent')
     }
-
-
   }
 
 
   const opponentMove = () => {
-    // setTimeout(() => console.log("opponentMove!", position, next, status), 1000)
-    console.log("opponentMove!", position, next, status)
+    // setTimeout(() => {
+      if (status === 'opponent') {
+        let winningSquare = findWinningSquare(position, next)
+        let playerWinningSquare = findWinningSquare(position, previous)
+        let newPosition = position
+        // setTimeout(() => console.log("opponentMove!", position, next, status), 1000)
 
-    // let winningSquare = findWinningSquare(position, newNext)
-    // let playerWinningSquare = findWinningSquare(position, oldNext)
-    //
-    // if (winningSquare >= 0 ) {
-    //   console.log("a")
-    //   delay(() => {
-    //     position[winningSquare] = newNext
-    //
-    //     setGameState({
-    //     ...gameState,
-    //     position: position,
-    //     status: 'opponent-won'
-    //   })
-    // })
-    // } else if (playerWinningSquare >= 0 ) {
-    //   delay(() => {
-    //     position[playerWinningSquare] = newNext
-    //
-    //     setGameState({
-    //       ...gameState,
-    //       position: position,
-    //       next: oldNext,
-    //       status: isDraw(position) ? 'draw' : 'player'
-    //     })
-    //   })
-    // } else if (isGameWon(position)) {
-    //   setGameState({
-    //     ...gameState,
-    //     position: position,
-    //     status: "player-won"
-    //   })
-    //   console.log("You won!")
-    // } else if (isDraw(position)) {
-    //   console.log("draw")
-    //   setGameState({
-    //     ...gameState,
-    //     position: position,
-    //     status: "draw"
-    //   })
-    // } else {
-    //   delay(() => {
-    //     position[randomEmptySquare(position)] = newNext
-    //     setGameState({
-    //     ...gameState,
-    //     position: position,
-    //     next: oldNext
-    //   })})
-    // }
+        if (winningSquare >= 0 ) {
+
+          console.log("winning square found!")
+          newPosition[winningSquare] = next
+
+          setPosition(newPosition)
+          setStatus('opponent-won')
+
+        } else if (playerWinningSquare >= 0 ) {
+
+          console.log("Watch out! player winning square found!")
+          newPosition[playerWinningSquare] = next
+
+          setPosition(newPosition)
+          setNext(previous)
+          setPrevious(next)
+          setStatus(isDraw(newPosition) ? 'draw' : 'player')
+
+        } else if (isGameWon(position)) {
+
+            console.log("You won!")
+
+            setStatus("player-won")
+
+        } else if (isDraw(position)) {
+
+            console.log("draw")
+
+            setStatus("draw")
+
+        } else {
+            newPosition[randomEmptySquare(position)] = next
+            setPosition(newPosition)
+            setNext(previous)
+            setStatus('player')
+        }
+      }
+    // }, 1000)
   }
 
   return (<Board
@@ -98,6 +89,7 @@ const Game = ({id, active}) => {
             playerFirst={playerFirst}
             position={position}
             next={next}
+            previous={previous}
             status={status}
             changeGameState={changeGameState}
             active={active}
